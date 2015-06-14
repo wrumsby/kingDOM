d76dc1e3d0223f6dc27fea2dadfd304d17e90c95
Walter Rumsby <walter@rumsby.net>
2015-06-15 06:49:53 +1200

    v2

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 define([], function () {
   'use strict';
 
+  /**
+   * Returns an `Array` of `HTMLElement` matching the given CSS selector.
+   *
+   * @param {String} selector
+   * @returns {Array}
+   */
   return function (selector) {
-    return document.querySelector(selector);
+    var nodeList = document.querySelectorAll(selector);
+    var length = nodeList.length;
+    var result = [];
+    var i;
+
+    for (i = 0; i < length; i++) {
+      result.push(nodeList.item(0));
+    }
+
+    return result;
   };
 });
